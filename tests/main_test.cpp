1b379d91750b173b3d55aef34d17061608c66077
JNJYan <jjy20140825@gmail.com>
2021-04-29 16:52:22 +0800

    init

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-04-29 08:56:52
- * @LastEditTime: 2021-04-29 15:57:06
+ * @LastEditTime: 2021-04-29 16:19:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /conan-learn/main_test.cpp
@@ -11,7 +11,6 @@
 
 int main(int argc, char **argv) {
     std::cout << "this is a unit test." << std::endl;
-    // ::testing::InitGoogleTest(&argc, argv);
-    return 0;
-    // return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
---
+++ tests/tools_test.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-29 16:11:01
+ * @LastEditTime: 2021-04-29 16:48:40
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /conan-learn/tests/tools_test.cpp
+ */
+#include <gtest/gtest.h>
+
+#include "tools.h"
+#include <iostream>
+
+TEST(tools_test, tool) {
+    EXPECT_EQ(1, 1);
+    // std::cout << "tools test." << std::endl;
+}
