fec61192ff146979e1fe28e5479acb7ada35789b
jiangjunyan <jiangjunyan@bigo.sg>
2021-07-03 18:58:12 +0800

    modify cmakefile

---
+++ tests/proto_test.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Author: JNJYan
+ * @Date: 2021-07-02 12:18:58
+ * @LastEditors: JNJYan
+ * @LastEditTime: 2021-07-02 12:27:05
+ * @Description: file content
+ * @FilePath: /template/tests/proto_test.cpp
+ */
+#include <gtest/gtest.h>
+#include <proto/test.pb.h>
+#include <goolge/protobuf/arena.h>
+
+#include <iostream>
+
+// TEST(proto_test, user_info_test) {
+//     google::protobuf::Arena::Create<>
+//     int age =     
+//     // std::cout << "tools test." << std::endl;
+// }
